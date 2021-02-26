279fe8a72df4ebab9d35eaadd72e6d776d3ff102
Dan Boterashvili <dan.boterashvili@gmail.com>
2021-02-25 22:36:50 -0500

    return 200 response to webhook

--- servertest.js
+++ servertest.js
@@ -30,4 +30,5 @@ console.log('Running a GraphQL API server at http://localhost:4000/graphql');
 
 app.get('/trelloCallback', function (req, res) {
   console.log(req);
+  return res.status(200);
 });
