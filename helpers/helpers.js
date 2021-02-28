14d1816bc9944a332f9fd58f7e6f62703b99c5fa
Dan Boterashvili <dan.boterashvili@gmail.com>
2021-02-28 18:47:17 -0500

    remove helpers folder

--- helpers/helpers.js
+++
@@ -1,9 +0,0 @@
-const fetch = require('node-fetch');
-const Resource = require('../models/resource');
-const Checklist = require('../models/checklist');
-const mongoose = require('mongoose');
-require('dotenv').config();
-
-
-
-
